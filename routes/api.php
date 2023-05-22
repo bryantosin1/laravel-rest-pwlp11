6f5e908575c22034e1566a88ad57eb133d17e71d
bryantosin1 <bryantosin1@gmail.com>
2023-05-22 21:17:41 +0700

    praktikum1

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+Route::get('/hello',function(){
+    // $data = ["message"=>"hello word"];
+    // return response()->json($data);
+    return 'hello world';
+});
